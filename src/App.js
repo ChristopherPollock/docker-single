57b7392d9552f491b39e60924f65df9423a9e30e
manicman <manicman@users.noreply.github.com>
2020-05-02 17:44:35 -0400

    Changed some text successfully

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. This Text Proves I'm now deploying through benastalk successfully!
+          Edit <code>src/App.js</code> and save to reload. This Text Proves I'm now deploying through beanstalk successfully!
         </p>
         <a
           className="App-link"
